681eb3c212085d190fee84ae69a1087b8b837ba3
Christian Perez <christianperez280801@gmail.com>
2024-02-29 15:53:50 +0100

    docs(web): add a navbar and a burger menu and all HTMLs

---
+++ JS/script.js
@@ -0,0 +1,36 @@
+/* CREAMOS UN EVENTO JUNTO CON SU TRIGGER */
+
+
+/* DECLARAMOS PERSINAJES DE LA OBRA */
+var line1__bars = document.querySelector(".line1__bars-menu");
+var line2__bars = document.querySelector(".line2__bars-menu");
+var line3__bars = document.querySelector(".line3__bars-menu");
+
+
+function animateBars() {
+    console.log("Clicked!");
+    line1__bars.classList.toggle("activeline1__bars-menu");
+    line2__bars.classList.toggle("activeline2__bars-menu");
+    line3__bars.classList.toggle("activeline3__bars-menu");
+
+    var menuList = document.getElementById(`menuList`);
+
+    console.log(menuList);
+
+    if (menuList.classList.contains(`active`)) {
+        menuList.classList.remove(`active`)
+    } else {
+        menuList.classList.add(`active`);
+    }
+}
+
+var barsMenu = document.querySelector(".bars__menu")
+
+barsMenu.addEventListener("click", animateBars);
+
+/* 
+    var ??? = (si a == b)? cambiar a `???` : sino a `???`
+
+    Operador ternario, que dicta que var next color = (si current es igual a original) ? entonces cambiar el valor al color blanco, de lo contrario : cambiar al original 
+
+*/
